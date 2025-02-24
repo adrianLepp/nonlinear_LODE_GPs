fix weight matrix calculation in localgp

src/nonlinear_LODE_GPs/example/localgp_example.py
@@ -6,78 +6,63 @@ from sage.all import *
 import sage
 #https://ask.sagemath.org/question/41204/getting-my-own-module-to-work-in-sage/
 from sage.calculus.var import var
-from kernels import *
-from mean_modules import *
+from nonlinear_LODE_GPs.kernels import *
+from nonlinear_LODE_GPs.mean_modules import *
 import torch
 import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------------------
-from  lodegp import *
-from helpers import *
+from nonlinear_LODE_GPs.lodegp import optimize_gp
+from nonlinear_LODE_GPs.helpers import *
+from nonlinear_LODE_GPs.weighting import Gaussian_Weight
+from nonlinear_LODE_GPs.localgp import Sum_LODEGP
 
 torch.set_default_dtype(torch.float64)
 device = 'cpu'
 
 system_name = "nonlinear_watertank"
 
-optim_steps = 50
+optim_steps = 200
 
 u_0 = 0.2
 u_1 = 0.3
+controls = [u_0, u_1]#, u_1
 
 t0 = 0.0
 t1 = 1000.0
+timestamps = [0.0, t1]
 
 output_distance = False
 
-train_time = Time_Def(
-    t0, 
-    t1, 
-    step=10
-)
-
-test_time = Time_Def(
-    t0, 
-    t1, 
-    step=0.1
-)
+downsample = 100
+sim_time = Time_Def(t0, t1, step=0.1)
+train_time = Time_Def(t0, t1, step=sim_time.step*downsample)
+test_time = Time_Def(t0, t1, step=0.1)
 
 system = load_system(system_name)
 num_tasks = system.dimension
+system_matrices = []
+equilibriums = []
 
+for i in range(len(controls)):
+    system_matrix, x = system.get_ODEmatrix(controls[i])
+    system_matrices.append(system_matrix)
+    equilibriums.append(torch.tensor(x))
 
-system_matrix_0 , x0 = system.get_ODEmatrix(u_0)
-system_matrix_1 , x1 = system.get_ODEmatrix(u_1)
-
-system_matrices = [
-    system_matrix_0, 
-    system_matrix_1
-    ]
-equilibriums = [
-    torch.tensor(x0), 
-    torch.tensor(x1)
-    ]
 if output_distance is True:
-    centers = [
-        torch.tensor([x0]), 
-        torch.tensor([x1])
-    ]
+    centers = [torch.tensor([x_i]) for x_i in equilibriums]
 else:
-    centers  = [
-        torch.tensor([[t0]]), 
-        torch.tensor([[t1]])
-    ]
+    centers = [torch.tensor([[t_i]]) for t_i in timestamps]
 
-
-l  = 1
 #l = 2.65e-3
 #l = 44194
-w_func = Gaussian_Weight(centers[0],l)
-d = w_func.covar_dist(centers[1], w_func.center, square_dist=True)
-l = d*torch.sqrt(torch.tensor(2))/8
+# w_func = Gaussian_Weight(centers[0])
+# w_func.length = 1
+# d = w_func.covar_dist(centers[1], w_func.center, square_dist=True)
+# l = d*torch.sqrt(torch.tensor(2))/8
 # print(l)
 #l = 1000
-
+l = torch.tensor([[176776.6953]])
 # print(w_func.covar_dist(centers[1], w_func.center, square_dist=True))
 # print(w_func(centers[1]))
 
@@ -87,21 +72,16 @@ l = d*torch.sqrt(torch.tensor(2))/8
 
 #states_0 = State_Description(x_e, x_0)
 
-u = np.linspace(u_1 * system.param.u, u_1 * system.param.u, train_time.count,axis=-1)
+# u = np.linspace(u_1 * system.param.u, u_1 * system.param.u, train_time.count,axis=-1)
+u = np.ones((sim_time.count,1)) * u_1 * system.param.u
 
-
-train_x, train_y= simulate_system(system, equilibriums[0][0:system.state_dimension], train_time, u)
-
-#sol = solve_ivp(system.stateTransition, [train_time.start, train_time.end], x_0[0:system.state_dimension], method='RK45', t_eval=t_reference.numpy(), args=(u, train_time.step ), max_step=train_time.step)#, max_step=dt ,  atol = 1, rtol = 1
-
+_train_x, _train_y= simulate_system(system, equilibriums[0][0:system.state_dimension], sim_time, u)
+train_x, train_y = downsample_data(_train_x, _train_y, downsample)
 
 #x_sim_current = np.concatenate([sol.y.transpose()[1::], u_ref[1::]], axis=1)
 likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks, noise_constraint=gpytorch.constraints.Positive())
 model = Sum_LODEGP(train_x, train_y, likelihood, num_tasks, system_matrices, equilibriums, centers, weight_lengthscale=l, output_distance=output_distance)
 
-
-# mean_module = Equilibrium_Mean(states.equilibrium, num_tasks)
-# model = LODEGP(train_x, train_y, likelihood, num_tasks, system_matrix, mean_module)
 optimize_gp(model, optim_steps)
 
 test_x = test_time.linspace()
@@ -110,10 +90,11 @@ likelihood.eval()
 
 
 #output = model(test_x)
-with torch.no_grad():
-    output = likelihood(model(test_x))
+with torch.no_grad(): #gpytorch.settings.prior_mode(True)
+    output = likelihood(model(test_x)).mean
+    # output = model.mean_module(test_x)
 
 train_data = Data_Def(train_x.numpy(), train_y.numpy(), system.state_dimension, system.control_dimension)
-test_data = Data_Def(test_x.numpy(), output.mean.numpy(), system.state_dimension, system.control_dimension)
+test_data = Data_Def(test_x.numpy(), output.numpy(), system.state_dimension, system.control_dimension)
 
-plot_results(train_data, test_data, equilibrium=x1)
+plot_results(train_data, test_data, equilibrium=equilibriums[-1])#,, equilibrium=equilibriums[4]

src/nonlinear_LODE_GPs/localgp.py
@@ -33,7 +33,11 @@ class Sum_LODEGP(gpytorch.models.ExactGP):
             kernels.append(LODE_Kernel(A_list[i], self.common_terms))
             #_kernels.append(Local_Kernel(LODE_Kernel(A_list[i], self.common_terms), num_tasks, center_list[i], weight_lengthscale, output_distance))
             means.append(Equilibrium_Mean(equilibrium_list[i], num_tasks))
-            w_functions.append(Gaussian_Weight(center_list[i], weight_lengthscale))
+            # w_fcts[i].initialize(length=torch.tensor(weight_lengthscale, requires_grad=True))#TODO
+            
+            w_functions.append(Gaussian_Weight(center_list[i])) #TODO  Constant_Weight()
+            w_functions[i].length = weight_lengthscale
+            # w_functions[i].initialize(weight=torch.tensor(1/len(A_list), requires_grad=False))
 
         #self.mean_module = Global_Mean(means, num_tasks,output_distance)
         self.mean_module = Global_Mean_2(means, w_functions, num_tasks,output_distance)
@@ -58,10 +62,10 @@ def plot_weights(x, weights, title="Weighting Function"):
     plt.figure(figsize=(12, 6))
     if isinstance(weights, list):
         for i, weight in enumerate(weights):
-            plt.plot(x, weight, label=f'Weight {i}')
+            plt.plot(x, weight.detach().numpy(), label=f'Weight {i}')
         plt.legend()
     else:
-        plt.plot(x, weights)
+        plt.plot(x, weights.detach().numpy())
     plt.xlabel("x")
     plt.ylabel("Weight")
     plt.title(title)
@@ -255,14 +259,18 @@ class Global_Kernel_2(Kernel):
                 distance_measure_1 = out[:length_1]
                 distance_measure_2 = out[-length_2:]
 
-        weight_matrices_1 = [torch.tile(weighting_function(distance_measure_1),(self.num_tasks,1)) for weighting_function in self.weight_functions]
-        weight_matrices_2 = [torch.tile(weighting_function(distance_measure_2),(self.num_tasks,1)) for weighting_function in self.weight_functions]
-
         covariances = [local_kernel(x1, x2, diag=False, **params) for local_kernel in self.local_kernels]
+        
+        # weight_matrices_1 = [torch.tile(weighting_function(distance_measure_1),(self.num_tasks,1)) for weighting_function in self.weight_functions]
+        # weight_matrices_2 = [torch.tile(weighting_function(distance_measure_2),(self.num_tasks,1)) for weighting_function in self.weight_functions]
+        weights_extended_1 = [weight_function(distance_measure_1).repeat_interleave(self.num_tasks).unsqueeze(1) for weight_function in self.weight_functions]
+        weights_extended_2 = [weight_function(distance_measure_2).repeat_interleave(self.num_tasks).unsqueeze(0) for weight_function in self.weight_functions] 
 
+        # weight = sum((weighting_function(distance_measure_1) * weighting_function(distance_measure_2).t())  for weighting_function in self.weight_functions)
+        # weight_matrix = torch.tile(weight,(self.num_tasks,self.num_tasks))
+        weight_matrix = sum(weights_extended_1[i] * weights_extended_1[i]  for i in range(len(weights_extended_1)))
 
-        weight = sum((weighting_function(distance_measure_1) * weighting_function(distance_measure_2).t())  for weighting_function in self.weight_functions)
-        weight_matrix = torch.tile(weight,(self.num_tasks,self.num_tasks))
-        covar = sum(weight_matrices_1[i] * covariances[i] * weight_matrices_2[i].t()  for i in range(len(covariances))) / weight_matrix
+        # covar = sum(weight_matrices_1[i] * covariances[i] * weight_matrices_2[i].t()  for i in range(len(covariances))) / weight_matrix
+        covar = sum(weights_extended_1[i] * covariances[i] * weights_extended_2[i]  for i in range(len(covariances))) / weight_matrix
 
         return covar